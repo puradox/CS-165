5c10d8119aa78ae33cefc88a3099e961ea3e491e
Sam Balana <sambalana247@gmail.com>
2018-04-02 22:06:55 -0700

    feat: Preliminary work for quickselect

--- select/doalg.c
+++ select/doalg.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+typedef unsigned short uint; // 0 to 65,535
+
+// Rand returns a random number between 1 and the specified max.
+// This is used to get the random pivot for use in QuickSelect.
+int rand(int max)
+{
+    return dshrandom(0) * max + 1;
+}
+
+// Performs Quick Select and stores the k largest elements in
+// the specified "Best" array.
+// Returns 1 if there were no errors, 0 otherwise.
+//
+// Bounds:
+//   10 <= n <= 10000
+//   1  <= k <= 100
 int doalg(int n, int k, int Best[])
 {
-    return 1;
+    printf("n=%d, k=%d\n", n, k);
+
+    // Since we don't have access to the actual values, we need to have
+    // some sort of representation for them. I will choose to represent
+    // them by their indices.
+    uint* elements = malloc(sizeof(uint)*k);
+    for (uint i = 0; i < n; i++) {
+        elements[i] = i + 1;
+    }
+
+    return 1; // Return with no errors for the time being.
 }
