938be3ebea6c9c747ceb3f074f8e278172d0ac22
Sam Balana <sambalana247@gmail.com>
2018-04-15 17:52:35 -0700

    feat(bst): Sort first k values

--- bst/doalg.c
+++ bst/doalg.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <math.h>
 
+#include "../common/heap.h"
 #include "../common/bst.h"
 #include "../common/util.h"
 
@@ -22,14 +23,14 @@ int doalg(int n, int k, int Best[])
     for (uint16_t i = 0; i < n; i++)
         elements[i] = i + 1;
 
-    // Sort the k maximum values
-    //maxHeapSelect(elements, 0, (uint16_t)(n - 1), (uint16_t)(n - k));
+    // Sort the first k values
+    maxHeapSort(elements, k);
     //printf("Comparisons: %d\n", getComps());
     //resetComps();
 
     // Copy results over
-    for (int i = n - 1; i >= n - k; i--)
-        Best[n - i - 1] = elements[i];
+    for (int i = k - 1; i >= 0; i--)
+        Best[k - 1 - i] = elements[i];
 
     free(elements);
     return 1; // No errors
