6c4e3eaa8c3ecb7e8a906a12871ddaae8f6c06f4
Sam Balana <sambalana247@gmail.com>
2018-04-17 01:36:19 -0700

    fix(sorting): Duplicate declaration of compare

--- common/sorting.c
+++ common/sorting.c
@@ -3,21 +3,21 @@
 
 #include "COMPARE.h"
 
-#ifdef DEBUG
-CompFunc compare = gt;
-#else
-CompFunc compare = compareGt;
-#endif
-
 void insertionSort(int arr[], int len)
 {
-  for (int i = 1; i < len; i++)
-  {
-    for (int j = i; j != 0 && compare(arr[j], arr[j - 1]); j--)
+#ifdef DEBUG
+    CompFunc compare = gt;
+#else
+    CompFunc compare = compareGt;
+#endif
+
+    for (int i = 1; i < len; i++)
     {
-      int temp = arr[j];
-      arr[j] = arr[j - 1];
-      arr[j - 1] = temp;
+        for (int j = i; j != 0 && compare(arr[j], arr[j - 1]); j--)
+        {
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+        }
     }
-  }
 }
