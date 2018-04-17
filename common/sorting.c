9c9f7b5ac9a808d40c9e311598cc3e139ab07933
Dallas-J <dallasajohnson14@gmail.com>
2018-04-17 01:23:56 -0700

    refactor(sorting): changed while loop to for loop

--- common/sorting.c
+++ common/sorting.c
@@ -13,13 +13,11 @@ void insertionSort(int arr[], int len)
 {
   for (int i = 1; i < len; i++)
   {
-    int j = i;
-    while (j != 0 && compare(arr[j], arr[j - 1]))
+    for (int j = i; j != 0 && compare(arr[j], arr[j - 1]); j--)
     {
       int temp = arr[j];
       arr[j] = arr[j - 1];
       arr[j - 1] = temp;
-      j--;
     }
   }
 }
