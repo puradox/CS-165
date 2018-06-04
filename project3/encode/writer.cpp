aa565203b875f11be8c94bb3d2444a9c907267eb
Dallas-J <dallasajohnson14@gmail.com>
2018-06-03 22:05:15 -0700

    feat(writer): Finish writer implementation

--- project3/encode/writer.cpp
+++ project3/encode/writer.cpp
@@ -23,31 +23,34 @@ void write(config conf, std::vector<encode_output> outputs, std::string filename
 
     for (encode_output out : outputs) {
         if (out.length == 0) {
+            // Write match length L (which is all zeros in this case)
             for (uint8_t i = 0; i < conf.L; i++) {
                 buffer->set(counter, 0);
                 ++counter;
-                if (counter == 8) {
-                    file.write((char*)buffer, 1);
-                    counter = 0;
-                }
-            }
-            
-            std::bitset<5> string_length(out.offset);
-            uint8_t strlen_offset = 5 - conf.S;
-            for (uint8_t i = 0; i < conf.S; i++) {
-                buffer->set(counter, string_length[strlen_offset + i]);
-                ++counter;
-                if (counter == 8) {
+                if (counter >= 8) {
                     file.write((char*)buffer, 1);
                     counter = 0;
                 }
             }
 
+            // Write string length S
+            std::bitset<5> string_length(out.offset);
+            uint8_t strlen_offset = 5 - conf.S;
+            for (uint8_t i = 0; i < conf.S; i++) {
+                buffer->set(counter, string_length[strlen_offset + i]);
+                ++counter;
+                if (counter >= 8) {
+                    file.write((char*)buffer, 1);
+                    counter = 0;
+                }
+            }
+
+            // Write the string contents
             for (char c : out.chars) {
                 std::bitset<8> char_buffer(c);
                 for (uint8_t i = 0; i < 8; i++) {
                     buffer->set(counter, char_buffer[i]);
-                    if (counter == 8) {
+                    if (counter >= 8) {
                         file.write((char*)buffer, 1);
                         counter = 0;
                     }
@@ -55,13 +58,41 @@ void write(config conf, std::vector<encode_output> outputs, std::string filename
             }
         }
         else {
-            std::bitset<14> length(out.length);
-            uint8_t length_offset = 14 - conf.L;
-            // TODO: write L & N
+            // Write the match length L
+            std::bitset<5> length(out.length);
+            uint8_t length_offset = 5 - conf.L;
+            for (uint8_t i = 0; i < conf.L; i++) {
+                buffer->set(counter, length[length_offset + i]);
+                ++counter;
+                if (counter >= 8) {
+                    file.write((char*)buffer, 1);
+                    counter = 0;
+                }
+            }
+
+            // Write the match offset N
+            std::bitset<14> offset(out.offset);
+            uint8_t offset_offset = 14 - conf.N;
+            for (uint8_t i = 0; i < conf.N; i++) {
+                buffer->set(counter, offset[offset_offset + i]);
+                ++counter;
+                if (counter >= 8) {
+                    file.write((char*)buffer, 1);
+                    counter = 0;
+                }
+            }
         }
     }
 
-    // TODO: Add end-of-file identifier
+    // Write the end-of-file token.
+    for (uint8_t i = 0; i < conf.L + conf.S; i++) {
+        buffer->set(counter, 0);
+        ++counter;
+        if (counter >= 8) {
+            file.write((char*)buffer, 1);
+            counter = 0;
+        }
+    }
 
     // If we have a partial byte left over, we have to write it. We don't care
     // what the remainder of the buffer is filled with since it is after our
